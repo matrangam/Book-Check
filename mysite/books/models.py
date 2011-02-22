91989f0627a3a954e63ac1dc69664adcefb2b08b
Meg Havern <meg@dmgctrl.com>
2011-02-22 18:03:06 -0500

    new db

--- mysite/books/models.py
+++ mysite/books/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
+class Topic(models.Model):
+	tag = models.CharField(max_length=25)
+	def __unicode__(self):
+		return self.tag
+
 class Book(models.Model):
 	title = models.CharField(max_length=500)
 	author = models.CharField(max_length=100)
 	quantity = models.IntegerField()
-	topic = models.ForeignKey(Topic)
+	topic = models.ManyToManyField(Topic)
 	description = models.TextField(max_length=500)
 	def __unicode__(self):
 		return self.title
 
 
 
-
-class Topic(models.Model):
-	tag = models.CharField(max_length=25)
-	def __unicode__(self):
-		return self.tag
 
