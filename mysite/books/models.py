33f418af619d51a13b224c3e4393ed5d18df975b
Mike Matranga <mamatranga@gmail.com>
2011-02-28 18:21:22 -0500

    added time stamp and cleaned up some displays

--- mysite/books/models.py
+++ mysite/books/models.py
@@ -10,9 +10,11 @@ class Book(models.Model):
     title = models.CharField(max_length=500)
     author = models.CharField(max_length=100)
     quantity = models.IntegerField()
-    topic = models.ManyToManyField('Topic')
     description = models.TextField(max_length=500)
+    check_date = models.DateTimeField(default=None, blank=True, null=True)
+    
     users = models.ManyToManyField(User, blank=True, null=True)
-
+    topic = models.ManyToManyField('Topic')
+    
     def __unicode__(self):
         return self.title
--- mysite/books/views.py
+++ mysite/books/views.py
@@ -4,7 +4,9 @@ from django.core.urlresolvers import reverse
 from django.template import RequestContext, Context, loader
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
+# 
+from datetime import datetime
 # 
 from books.models import *
 from books.forms import *
@@ -12,12 +14,14 @@ from books.forms import *
 @login_required
 def list(request):
     book = Book.objects.all()
-    return render_to_response('list.html', {'book': book})
+    user = Book.users
+    return render_to_response('list.html', {'book': book, 'user': request.user})
 
 
-def users(request, user_id=None):
-    users = User.objects.all()
-    return render_to_response('users.html', {'users':users})
+def users(request):
+    book = Book.users
+    user = Book.users
+    return render_to_response('users.html', {'user':user, 'book':book})
 
 
 @login_required
@@ -33,13 +37,16 @@ def user_detail(request, user_id):
 
 
 @login_required
+@permission_required('mysite.add_book')
 def add_book(request, book_id=None):
     book = get_object_or_404(Book, pk=book_id) if book_id else None 
     if request.method == 'POST':
         form = NewBookForm(request.POST, request.FILES, instance=book)
         if form.is_valid():
+            book = form.save(commit=False)
+            book.check_date = datetime.now()
             book = form.save()
-            return redirect('bookcheck:list')
+            return redirect('bookcheck:list', book_id=book.id)
     else:
         form = NewBookForm(instance=book)        
     return render_to_response('add_book.html', { 'form':form, 'book':book }, RequestContext(request))
@@ -56,6 +63,7 @@ def new_user(request, user_id=None):
         form = NewUserForm(instance=user)
     return render_to_response('new_user.html', { 'form':form, 'user':user }, RequestContext(request))                 
 
+
 @login_required
 def checkout(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
@@ -63,9 +71,10 @@ def checkout(request, book_id):
     if request.method == 'POST' and book.quantity > 0:
         if request.user not in book.users.all():
             book.quantity = book.quantity - 1
+            book.check_date = datetime.now()
             book.save()
             book.users.add(request.user)
-        return redirect('bookcheck:detail', book_id=book_id)
+        return redirect('bookcheck:detail', book_id)
     
     return render_to_response('checkout.html', { 'book':book, 'user':request.user }, RequestContext(request))    
 
@@ -77,6 +86,6 @@ def returned(request, book_id):
             book.quantity = book.quantity + 1
             book.save()
             book.users.remove(request.user)
-        return redirect('bookcheck:detail', book_id)
+        return redirect('bookcheck:detail', book_id=book_id)
         
     return render_to_response('returned.html', { 'book':book }, RequestContext(request)) 
